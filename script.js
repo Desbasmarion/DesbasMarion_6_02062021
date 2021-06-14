236e2e62cb23def04d6e834b0d8f0944ae1a5414
Marion Desbas <desbasmarion@yahoo.fr>
2021-06-14 17:59:05 +0200

    fin css index + debut html Mimi keel

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+/*tags
+Si un tags selectionné dans le banner est présent dans une fiche photographes, alors afficher toutes les fiches photographes concernées
+*/
 
+/*methodo
+1- integration html/css
+2- accessibilité
+3- Js
+*/
